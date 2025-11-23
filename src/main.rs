ccb609c5104f3dbe2895b33a5a2e735127689040
Mikura_21 <nocturne@mikura-21.com>
2025-11-23 15:29:46 +0900

    3.1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::io::{self, Write};
+
+mod color;
 mod vec3;
+
+use color::Color;
 
 fn main() {
     // Image
@@ -19,11 +23,9 @@ fn main() {
             let g = j as f64 / (image_height - 1) as f64;
             let b = 0.0;
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
+            let pixel_color = Color::new(r, g, b);
 
-            println!("{} {} {}", ir, ig, ib);
+            color::write_color(&pixel_color);
         }
     }
     eprintln!("\nDone.                 ");
