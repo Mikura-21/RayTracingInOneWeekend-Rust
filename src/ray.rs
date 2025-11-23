dc3a6dba6a2d562535486bf930fbe32b0cccd4c8
Mikura_21 <nocturne@mikura-21.com>
2025-11-23 15:39:51 +0900

    4.1

---
+++ src/ray.rs
@@ -0,0 +1,45 @@
+use crate::vec3::{Point3, Vec3};
+
+#[derive(Debug, Clone, Copy)]
+pub struct Ray {
+    pub orig: Point3,
+    pub dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Ray {
+            orig: origin,
+            dir: direction,
+        }
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.orig + t * self.dir
+    }
+}
+
+// #ifndef RAY_H
+// #define RAY_H
+//
+// #include "vec3.h"
+//
+// class ray {
+//   public:
+//     ray() {}
+//
+//     ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+//
+//     const point3& origin() const  { return orig; }
+//     const vec3& direction() const { return dir; }
+//
+//     point3 at(double t) const {
+//         return orig + t*dir;
+//     }
+//
+//   private:
+//     point3 orig;
+//     vec3 dir;
+// };
+//
+// #endif
