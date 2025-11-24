ff2b6025c45b62bd574348887bfd65487688a9ad
Mikura_21 <nocturne@mikura-21.com>
2025-11-24 20:07:02 +0900

    5.2

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,21 @@ use color::Color;
 use crate::ray::Ray;
 use crate::vec3::{Point3, Vec3};
 
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc: Vec3 = center - r.orig;
+    let a = r.dir.dot(r.dir);
+    let b = -2.0 * r.dir.dot(oc);
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant >= 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = r.dir.unit_vector();
     let a = 0.5 * (unit_direction.y + 1.0);
     (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
