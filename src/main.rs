961b456344c843737c86d3f6dfe41f2ccbbe10ee
Mikura_21 <nocturne@mikura-21.com>
2025-11-27 22:56:49 +0900

    10.5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 mod camera;
 mod color;
 mod hittable;
@@ -9,15 +11,40 @@ mod sphere;
 mod vec3;
 
 use crate::camera::Camera;
+use crate::color::Color;
 use crate::hittable_list::HittableList;
+use crate::material::{Lambertian, Metal};
 use crate::sphere::Sphere;
 use crate::vec3::Point3;
 
 fn main() {
     let mut world = HittableList::new();
 
-    world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
-    world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+    let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        material_ground,
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.2),
+        0.5,
+        material_center,
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
+        material_left,
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
+        material_right,
+    )));
 
     let aspect_ratio = 16.0 / 9.0;
     let image_width: usize = 400;
