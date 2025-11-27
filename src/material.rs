82144c6ad86b53dc94f78c5e1b1fc197a113513b
Mikura_21 <nocturne@mikura-21.com>
2025-11-27 23:48:38 +0900

    11.2

--- src/material.rs
+++ src/material.rs
@@ -57,3 +57,33 @@ impl Material for Metal {
         (scattered.dir.dot(rec.normal) > 0.0).then_some((attenuation, scattered))
     }
 }
+
+pub struct Dielectric {
+    // Refractive index in vacuum or air, or the ratio of the material's refractive index over
+    // the refractive index of the enclosing media
+    pub refraction_index: f64,
+}
+
+impl Dielectric {
+    pub fn new(refraction_index: f64) -> Self {
+        Self { refraction_index }
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord, rng: &mut SmallRng) -> Option<(Color, Ray)> {
+        let attenuation = Color::new(1.0, 1.0, 1.0);
+        let ri = if rec.front_face {
+            1.0 / self.refraction_index
+        } else {
+            self.refraction_index
+        };
+
+        let unit_direction = r_in.dir.unit_vector();
+        let refracted = unit_direction.refract(rec.normal, ri);
+
+        let scattered = Ray::new(rec.p, refracted);
+
+        Some((attenuation, scattered))
+    }
+}
