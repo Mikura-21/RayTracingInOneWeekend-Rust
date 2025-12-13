d4b6cff371cff75cf32219903fc0b1cbd378aa84
Mikura_21 <nocturne@mikura-21.com>
2025-12-13 20:27:04 +0900

    The Next Week 2.1

--- src/ray.rs
+++ src/ray.rs
@@ -4,13 +4,15 @@ use crate::vec3::{Point3, Vec3};
 pub struct Ray {
     pub orig: Point3,
     pub dir: Vec3,
+    tm: f64,
 }
 
 impl Ray {
-    pub fn new(origin: Point3, direction: Vec3) -> Self {
+    pub fn new(origin: Point3, direction: Vec3, time: f64) -> Self {
         Ray {
             orig: origin,
             dir: direction,
+            tm: time,
         }
     }
 
