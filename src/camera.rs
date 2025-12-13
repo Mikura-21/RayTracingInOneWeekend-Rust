07fc2777053a7008889bd41f48bc0298c2946a22
Mikura_21 <nocturne@mikura-21.com>
2025-12-13 20:29:09 +0900

    2.3

--- src/camera.rs
+++ src/camera.rs
@@ -172,8 +172,9 @@ impl Camera {
             self.defocus_disk_sample(rng)
         };
         let ray_direction = pixel_sample - ray_origin;
+        let ray_time = rng.random_range(0.0..1.0);
 
-        Ray::new(ray_origin, ray_direction)
+        Ray::new(ray_origin, ray_direction, ray_time)
     }
 
     fn sample_square(&self, rng: &mut SmallRng) -> Vec3 {
