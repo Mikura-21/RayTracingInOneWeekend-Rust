5eb9fad78e605af898fe6be6c793e30981b1f63a
Mikura_21 <nocturne@mikura-21.com>
2026-04-23 15:23:49 +0900

    2.6

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() {
     let mut world = HittableList::new();
 
     let ground_material = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
-    world.add(Box::new(Sphere::new(
+    world.add(Box::new(Sphere::new_stationary(
         Point3::new(0.0, -1000.0, 0.0),
         1000.0,
         ground_material,
@@ -46,46 +46,60 @@ fn main() {
                     let albedo =
                         Color::random(&mut rng, 0.0, 1.0) * Color::random(&mut rng, 0.0, 1.0);
                     let sphere_material = Rc::new(Lambertian::new(albedo));
-                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                    let center2 = center + Vec3::new(0.0, rng.random_range(0.0..0.5), 0.0);
+                    world.add(Box::new(Sphere::new_moving(
+                        center,
+                        center2,
+                        0.2,
+                        sphere_material,
+                    )));
                 } else if choose_mat < 0.95 {
                     // metal
                     let albedo = Color::random(&mut rng, 0.5, 1.0);
                     let fuzz = rng.random::<f64>() * 0.5;
                     let sphere_material = Rc::new(Metal::new(albedo, fuzz));
-                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                    world.add(Box::new(Sphere::new_stationary(
+                        center,
+                        0.2,
+                        sphere_material,
+                    )));
                 } else {
                     // glass
                     let sphere_material = Rc::new(Dielectric::new(1.5));
-                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                    world.add(Box::new(Sphere::new_stationary(
+                        center,
+                        0.2,
+                        sphere_material,
+                    )));
                 }
             }
         }
     }
 
     let material1 = Rc::new(Dielectric::new(1.5));
-    world.add(Box::new(Sphere::new(
+    world.add(Box::new(Sphere::new_stationary(
         Point3::new(0.0, 1.0, 0.0),
         1.0,
         material1,
     )));
 
     let material2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
-    world.add(Box::new(Sphere::new(
+    world.add(Box::new(Sphere::new_stationary(
         Point3::new(-4.0, 1.0, 0.0),
         1.0,
         material2,
     )));
 
     let material3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
-    world.add(Box::new(Sphere::new(
+    world.add(Box::new(Sphere::new_stationary(
         Point3::new(4.0, 1.0, 0.0),
         1.0,
         material3,
     )));
 
     let aspect_ratio = 16.0 / 9.0;
-    let image_width: usize = 1200;
-    let samples_per_pixel: usize = 500;
+    let image_width: usize = 400;
+    let samples_per_pixel: usize = 100;
     let max_depth: usize = 50;
 
     let vfov = 20.0;
