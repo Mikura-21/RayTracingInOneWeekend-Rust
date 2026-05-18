7247383cdf03e82b9a2486286ecd0bd8943f55d4
Mikura_21 <nocturne@mikura-21.com>
2026-05-18 15:25:16 +0900

    6.3

--- src/quad.rs
+++ src/quad.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use crate::aabb::Aabb;
 use crate::hittable::{HitRecord, Hittable};
 use crate::interval::Interval;
@@ -9,18 +11,28 @@ pub struct Quad {
     q: Point3,
     u: Vec3,
     v: Vec3,
+    w: Vec3,
     mat: MaterialPtr,
     bbox: Aabb,
+    normal: Vec3,
+    d: f64,
 }
 
 impl Quad {
     pub fn new(q: Point3, u: Vec3, v: Vec3, mat: MaterialPtr) -> Self {
+        let n = u.cross(v);
+        let w = n / n.dot(n);
+        let normal = n.unit_vector();
+        let d = normal.dot(q);
         Self {
             q: q,
             u: u,
             v: v,
+            w: w,
             mat: mat,
             bbox: Self::compute_bounding_box(q, u, v),
+            normal: normal,
+            d: d,
         }
     }
 
@@ -34,7 +46,30 @@ impl Quad {
 
 impl Hittable for Quad {
     fn hit(&self, r: &Ray, ray_t: Interval) -> Option<HitRecord> {
-        None // To be implemented
+        let denom = self.normal.dot(r.dir);
+
+        // No hit if the ray is parallel to the plane.
+        if denom.abs() < 1e-8 {
+            return None;
+        }
+
+        // Return false if the hit point parameter t is outside the ray interval.
+        let t = (self.d - self.normal.dot(r.orig)) / denom;
+        if !ray_t.contains(t) {
+            return None;
+        }
+
+        let intersection = r.at(t);
+
+        Some(HitRecord::new(
+            intersection,
+            t,
+            0.0,
+            0.0,
+            r,
+            self.normal,
+            Arc::clone(&self.mat),
+        ))
     }
 
     fn bounding_box(&self) -> Aabb {
