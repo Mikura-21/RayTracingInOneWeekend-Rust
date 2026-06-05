96ad90c844d55c3e45fbb80011bfb87280b6a4bf
Mikura_21 <nocturne@mikura-21.com>
2026-06-05 23:43:04 +0900

    7.1

--- src/material.rs
+++ src/material.rs
@@ -1,17 +1,24 @@
+use rand::RngExt;
+use rand::rngs::SmallRng;
 use std::sync::Arc;
 
 use crate::color::Color;
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
-use rand::RngExt;
-use rand::rngs::SmallRng;
+use crate::vec3::Point3;
 
 use crate::texture::{SolidColor, TexturePtr};
 
 pub type MaterialPtr = Arc<dyn Material + Send + Sync>;
 
 pub trait Material {
-    fn scatter(&self, r_in: &Ray, rec: &HitRecord, rng: &mut SmallRng) -> Option<(Color, Ray)>;
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord, rng: &mut SmallRng) -> Option<(Color, Ray)> {
+        None
+    }
+
+    fn emitted(&self, _u: f64, _v: f64, _p: Point3) -> Color {
+        Color::new(0.0, 0.0, 0.0)
+    }
 }
 
 pub struct Lambertian {
@@ -24,7 +31,7 @@ impl Lambertian {
             tex: Arc::new(SolidColor::new(albedo)),
         }
     }
-    
+
     pub fn from_texture(tex: TexturePtr) -> Self {
         Self { tex }
     }
@@ -81,6 +88,13 @@ impl Dielectric {
     pub fn new(refraction_index: f64) -> Self {
         Self { refraction_index }
     }
+
+    fn reflectance(cosine: f64, refraction_index: f64) -> f64 {
+        // Use Schlick's approximation for reflectance.
+        let r0 = (1.0 - refraction_index) / (1.0 + refraction_index);
+        let r0 = r0 * r0;
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
 }
 
 impl Material for Dielectric {
@@ -112,11 +126,22 @@ impl Material for Dielectric {
     }
 }
 
-impl Dielectric {
-    fn reflectance(cosine: f64, refraction_index: f64) -> f64 {
-        // Use Schlick's approximation for reflectance.
-        let r0 = (1.0 - refraction_index) / (1.0 + refraction_index);
-        let r0 = r0 * r0;
-        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+pub struct DiffuseLight {
+    tex: TexturePtr,
+}
+
+impl DiffuseLight {
+    pub fn new(tex: TexturePtr) -> Self {
+        Self { tex }
+    }
+
+    pub fn from_color(color: Color) -> Self {
+        Self {
+            tex: Arc::new(SolidColor::new(color)),
+        }
+    }
+
+    pub fn emitted(&self, u: f64, v: f64, p: Point3) -> Color {
+        self.tex.value(u, v, p)
     }
 }
