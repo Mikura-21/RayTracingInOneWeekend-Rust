599951516b5690335d2d6db19b8cdfb74320bd1b
Mikura_21 <nocturne@mikura-21.com>
2026-08-02 06:37:37 +0900

    10

--- src/main.rs
+++ src/main.rs
@@ -616,8 +616,149 @@ fn cornell_smoke() {
     cam.render(&world);
 }
 
+fn final_scene(image_width: usize, samples_per_pixel: usize, max_depth: usize) {
+    let mut rng = SmallRng::from_rng(&mut rand::rng());
+
+    let mut boxes1 = HittableList::new();
+    let ground = Arc::new(Lambertian::new(Color::new(0.48, 0.83, 0.53)));
+
+    let boxes_per_side = 20;
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = rng.random_range(1.0..101.0);
+            let z1 = z0 + w;
+
+            boxes1.add(Arc::new(make_box(
+                Point3::new(x0, y0, z0),
+                Point3::new(x1, y1, z1),
+                ground.clone(),
+            )));
+        }
+    }
+
+    let mut world = HittableList::new();
+    world.add(Arc::new(BvhNode::from_list(boxes1)));
+
+    let light = Arc::new(DiffuseLight::from_color(Color::new(7.0, 7.0, 7.0)));
+    world.add(Arc::new(Quad::new(
+        Point3::new(123.0, 554.0, 147.0),
+        Vec3::new(300.0, 0.0, 0.0),
+        Vec3::new(0.0, 0.0, 265.0),
+        light,
+    )));
+
+    let center1 = Point3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let shpere_material = Arc::new(Lambertian::new(Color::new(0.7, 0.3, 0.1)));
+    world.add(Arc::new(Sphere::new_moving(
+        center1,
+        center2,
+        50.0,
+        shpere_material,
+    )));
+
+    world.add(Arc::new(Sphere::new_stationary(
+        Point3::new(260.0, 150.0, 45.0),
+        50.0,
+        Arc::new(Dielectric::new(1.5)),
+    )));
+    world.add(Arc::new(Sphere::new_stationary(
+        Point3::new(0.0, 150.0, 145.0),
+        50.0,
+        Arc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.0)),
+    )));
+
+    let boundary = Arc::new(Sphere::new_stationary(
+        Point3::new(360.0, 150.0, 145.0),
+        70.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+    world.add(boundary.clone());
+    world.add(Arc::new(ConstantMedium::from_color(
+        boundary,
+        0.2,
+        Color::new(0.2, 0.4, 0.9),
+    )));
+    let boundary = Arc::new(Sphere::new_stationary(
+        Point3::new(0.0, 0.0, 0.0),
+        5000.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+    world.add(Arc::new(ConstantMedium::from_color(
+        boundary,
+        0.0001,
+        Color::new(1.0, 1.0, 1.0),
+    )));
+
+    let emat = Arc::new(Lambertian::from_texture(Arc::new(ImageTexture::new(
+        "earthmap.jpg",
+    ))));
+    world.add(Arc::new(Sphere::new_stationary(
+        Point3::new(400.0, 200.0, 400.0),
+        100.0,
+        emat,
+    )));
+    let pertext = Arc::new(NoiseTexture::new(&mut rng, 0.2));
+    world.add(Arc::new(Sphere::new_stationary(
+        Point3::new(220.0, 280.0, 300.0),
+        80.0,
+        Arc::new(Lambertian::from_texture(pertext)),
+    )));
+
+    let mut boxes2 = HittableList::new();
+    let white = Arc::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for _ in 0..ns {
+        boxes2.add(Arc::new(Sphere::new_stationary(
+            Point3::random(&mut rng, 0.0, 165.0),
+            10.0,
+            white.clone(),
+        )));
+    }
+
+    world.add(Arc::new(Translate::new(
+        Arc::new(RotateY::new(Arc::new(BvhNode::from_list(boxes2)), 15.0)),
+        Vec3::new(-100.0, 270.0, 395.0),
+    )));
+
+    let aspect_ratio = 1.0;
+    let image_width: usize = image_width;
+    let samples_per_pixel: usize = samples_per_pixel;
+    let max_depth: usize = max_depth;
+    let background = Color::new(0.0, 0.0, 0.0);
+
+    let vfov = 40.0;
+    let lookfrom = Point3::new(478.0, 278.0, -600.0);
+    let lookat = Point3::new(278.0, 278.0, 0.0);
+    let vup = Vec3::new(0.0, 1.0, 0.0);
+
+    let defocus_angle = 0.0;
+    let focus_dist = 10.0;
+
+    let cam = Camera::new(
+        aspect_ratio,
+        image_width,
+        samples_per_pixel,
+        max_depth,
+        background,
+        vfov,
+        lookfrom,
+        lookat,
+        vup,
+        defocus_angle,
+        focus_dist,
+    );
+
+    cam.render(&world);
+}
+
 fn main() {
-    match 8 {
+    match 0 {
         1 => bouncing_spheres(),
         2 => checkered_spheres(),
         3 => earth(),
@@ -626,6 +767,8 @@ fn main() {
         6 => simple_light(),
         7 => cornell_box(),
         8 => cornell_smoke(),
-        _ => {}
+        9 => final_scene(800, 10000, 40),
+        10 => final_scene(400, 250, 10),
+        _ => final_scene(400, 250, 4),
     }
 }
